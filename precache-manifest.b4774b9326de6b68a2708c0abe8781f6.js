1b00a1db1aae7cf15bd865bb13fcae229b77025d
Jurij-git <8good8@gmail.com>
2020-10-19 17:28:21 -0400

    Updates

---
+++ precache-manifest.b4774b9326de6b68a2708c0abe8781f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da510b755531d0bf08d206b3ad40c688",
+    "url": "/vincheck/index.html"
+  },
+  {
+    "revision": "f388c960a09feca9bb17",
+    "url": "/vincheck/static/css/main.8f812387.chunk.css"
+  },
+  {
+    "revision": "c70fe243910027efee0d",
+    "url": "/vincheck/static/js/2.0f6cd7b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vincheck/static/js/2.0f6cd7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f388c960a09feca9bb17",
+    "url": "/vincheck/static/js/main.7f174301.chunk.js"
+  },
+  {
+    "revision": "b6cd082ae1cc945c93df",
+    "url": "/vincheck/static/js/runtime-main.22ad14ae.js"
+  },
+  {
+    "revision": "90e83534d686d4a3773c914979efbdab",
+    "url": "/vincheck/static/media/loader.90e83534.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vincheck/static/media/logo.5d5d9eef.svg"
+  }
+]);
